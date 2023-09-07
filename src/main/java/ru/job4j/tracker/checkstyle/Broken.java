57d83d43f48bbe9e0bdeb63421c9cb8be74afa8f
Aleksandr-Purgin <aleksandr-purgin@inbox.ru>
2023-09-07 07:16:29 +0300

    логи1

--- src/main/java/ru/job4j/tracker/checkstyle/Broken.java
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -4,15 +4,19 @@ public class Broken {
     private int sizeOfEmpty = 10;
     public String surname;
     public static final String NEWVALUE = "";
+
     void echo() { }
 
     void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
+        return;
     }
+
     void method(int a, int b, int c, int d, int e, int f, int g, int h) {
     }
+
     String Name;
 
     Broken() {}
